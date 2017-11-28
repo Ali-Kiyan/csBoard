d55090665ae8e664c411461e67e6659f58d13f54
Ali <alikiyand@gmail.com>
2017-11-28 18:15:21 +0000

    UI prepration for workspace customization

--- csBoard/assets/js/chatScript.js
+++ csBoard/assets/js/chatScript.js
@@ -20,7 +20,7 @@
                 // Call the Send method on the hub. 
                 chat.server.send($('#displayname').val(), $('#message').val());
                 // Clear text box and reset focus for next comment. 
-                $('#message').val().focus();
+                $('#message').val('').focus();
             });
         });
     });
