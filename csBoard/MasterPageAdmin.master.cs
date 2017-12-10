0804f0e4c3f9d3bf9b3512ef06c566bf0d81d470
Ali <alikiyand@gmail.com>
2017-12-10 20:23:04 +0000

    fully functioning workspace customization with database

--- csBoard/MasterPageAdmin.master.cs
+++ csBoard/MasterPageAdmin.master.cs
@@ -6,14 +6,45 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
     SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["csBoardDBConnectionString"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
+   
         // read from db and then apply the customizat
         con.Open();
+        workspacesTableAdapters.work_SpaceTableAdapter workspaceHandle = new workspacesTableAdapters.work_SpaceTableAdapter();
+        workspaces.work_SpaceDataTable workspacestable = workspaceHandle.GetData();
+   
+        foreach (DataRow row in workspacestable.Rows)
+        {
+            if (System.Convert.ToString(Session["user_id"]) == System.Convert.ToString(row["user_id"]))
+            {
+                if (row["Font_Size"] != DBNull.Value)
+                {
+                    bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.FontSize, System.Convert.ToString(row["Font_Size"]) + "px");
+                 
+                }
+                if (row["Colour_Scheme"] != DBNull.Value)
+                {
+                    bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.BackgroundColor, System.Convert.ToString(row["Colour_Scheme"]));
+       
+                }
+                if (row["Direction"] != DBNull.Value)
+                {
+                    bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.Direction, System.Convert.ToString(row["Direction"]));
+           
+                }
+            }
+            else
+            {
+                continue;
+            }
+
+        }
        
     }
 
@@ -22,10 +53,8 @@ public partial class MasterPage : System.Web.UI.MasterPage
        
         SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Colour_Scheme,user_id) VALUES ('black',"+ Session["user_id"].ToString() + ")", con);
         cmd.ExecuteNonQuery();
-        string bg = "black";
-        bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.BackgroundColor, bg);
-        bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.FontWeight, "600");
         con.Close();
+        redirect();
     }
 
     protected void fontS_Click(object sender, EventArgs e)
@@ -33,24 +62,29 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
         SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Font_Size,user_id) VALUES (18," + Session["user_id"].ToString() + ")", con);
         cmd.ExecuteNonQuery();
-        bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.FontSize, "18px");
         con.Close();
+        redirect();
     }
 
     protected void rtl_Click(object sender, EventArgs e)
     {
         SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Direction,user_id) VALUES ('rtl'," + Session["user_id"].ToString() + ")", con);
         cmd.ExecuteNonQuery();
-        form1.Attributes.CssStyle.Add(HtmlTextWriterStyle.Direction, "rtl");
         con.Close();
+        redirect();
     }
 
     protected void ltr_Click(object sender, EventArgs e)
     {
         SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Direction,user_id) VALUES ('ltr'," + Session["user_id"].ToString() + ")", con);
         cmd.ExecuteNonQuery();
-        form1.Attributes.CssStyle.Add(HtmlTextWriterStyle.Direction, "ltr");
         con.Close();
+        redirect();
     }
+    protected void redirect()
+    {
+        Response.Redirect(Request.RawUrl);
+    }
+   
 
 }
