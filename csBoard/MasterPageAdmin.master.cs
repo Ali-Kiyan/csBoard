c6596d8a752b9bf0d74ac37c0661b9596fdf1746
Ali <alikiyand@gmail.com>
2017-12-10 00:55:50 +0000

    successful insert to work space with changing in db schema

--- csBoard/MasterPageAdmin.master.cs
+++ csBoard/MasterPageAdmin.master.cs
@@ -4,33 +4,50 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["csBoardDBConnectionString"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
         // read from db and then apply the customizat
+        con.Open();
+       
     }
 
     protected void dark_Click(object sender, EventArgs e)
     {
+       
+        SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Colour_Scheme,user_id) VALUES ('black',"+ Session["user_id"].ToString() + ")", con);
+        cmd.ExecuteNonQuery();
+        
         string bg = "black";
         bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.BackgroundColor, bg);
         bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.FontWeight, "600");
+        con.Close();
     }
 
     protected void fontS_Click(object sender, EventArgs e)
     {
+
+        SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Font_Size,user_id) VALUES (18," + Session["user_id"].ToString() + ")", con);
+        cmd.ExecuteNonQuery();
         bodyMain.Attributes.CssStyle.Add(HtmlTextWriterStyle.FontSize, "18px");
     }
 
     protected void rtl_Click(object sender, EventArgs e)
     {
+        SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Direction,user_id) VALUES ('rtl'," + Session["user_id"].ToString() + ")", con);
+        cmd.ExecuteNonQuery();
         form1.Attributes.CssStyle.Add(HtmlTextWriterStyle.Direction, "rtl");
     }
 
     protected void ltr_Click(object sender, EventArgs e)
     {
+        SqlCommand cmd = new SqlCommand("INSERT INTO work_Space (Direction,user_id) VALUES ('ltr'," + Session["user_id"].ToString() + ")", con);
+        cmd.ExecuteNonQuery();
         form1.Attributes.CssStyle.Add(HtmlTextWriterStyle.Direction, "ltr");
     }
 }
