931b9eed77b7cd836311c864f5fc2e2b7fa90026
Ali <alikiyand@gmail.com>
2017-12-12 01:14:31 +0000

    client UI- Themes(currently working)

--- csBoard/MasterPageClient.master.cs
+++ csBoard/MasterPageClient.master.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["csBoardDBConnectionString"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
+    {
+        // read from db and then apply the customize
+        con.Open();
+    }
+
+    protected void Theme1(object sender, EventArgs e)
     {
 
     }
