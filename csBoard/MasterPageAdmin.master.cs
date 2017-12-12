ca04589b7a0a1e4a6b348dac9d0a6c8942f83a93
Ali <alikiyand@gmail.com>
2017-12-12 01:48:13 +0000

    implement themes with problem with bodyMain

--- csBoard/MasterPageAdmin.master.cs
+++ csBoard/MasterPageAdmin.master.cs
@@ -14,7 +14,7 @@ public partial class MasterPage : System.Web.UI.MasterPage
     protected void Page_Load(object sender, EventArgs e)
     {
    
-        // read from db and then apply the customizat
+        // read from db and then apply the customise
         con.Open();
         workspacesTableAdapters.work_SpaceTableAdapter workspaceHandle = new workspacesTableAdapters.work_SpaceTableAdapter();
         workspaces.work_SpaceDataTable workspacestable = workspaceHandle.GetData();
